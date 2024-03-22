10f2b1ba11e092e1bda86bae16a844b0fca74241
Matúš <kollarm9@gmail.com>
2024-03-22 22:51:00 +0100

    Remove firebase-admin package Add getUserDocument function for firestore.ts

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -1,6 +1,20 @@
 import app from './config';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, collection, doc, getDoc, onSnapshot, addDoc, Timestamp, updateDoc } from 'firebase/firestore';
 
 // Initialize Firestore
 const db = getFirestore(app);
 
+const getUserDocument = async (uid: string) => {
+    if (!uid) return null;
+    const userDocumentReference = doc(db, 'users', uid);
+    const userDocument = await getDoc(userDocumentReference);
+
+    if (userDocument.exists()) {
+        return userDocument.data();
+    } else {
+        console.log('Uid is invalid.');
+        return null;
+    }
+};
+
+export { getUserDocument };
