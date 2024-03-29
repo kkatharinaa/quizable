abfe459786ba18a2f36a8308a9b94b34626988ec
sebi.tatar2@gmail.com <sebi.tatar2@gmail.com>
2024-03-29 20:57:31 +0100

    Convert TypeScript functions to static methods

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -4,57 +4,58 @@ import { getFirestore, doc, getDoc, collection, getDocs, onSnapshot } from 'fire
 // Initialize Firestore
 const db = getFirestore(app);
 
-// Gets the user document from the database
-const getUserDocument = async (uid: string) => {
-    const userDocumentReference = doc(db, 'users', uid);
-    const userDocument = await getDoc(userDocumentReference);
+export default class QuizFireStore {
 
-    if (userDocument.exists()) {
-        return userDocument.data();
-    } else {
-        console.log('Cannot get user.');
-        return null;
+    // Gets the user document from the database
+    static async getUserDocument(uid: string) {
+        const userDocumentReference = doc(db, 'users', uid);
+        const userDocument = await getDoc(userDocumentReference);
+
+        if (userDocument.exists()) {
+            return userDocument.data();
+        } else {
+            console.log('Cannot get user.');
+            return null;
+        }
     }
-};
 
-// Gets the whole collection of quizzes for a user
-const getUserQuizzes = async (uid: string) => {
-    const userDocumentReference = doc(db, 'users', uid);
-    const quizCollectionReference = collection(userDocumentReference, 'quizzes');
-    const quizCollection = await getDocs(quizCollectionReference);
+    // Gets the whole collection of quizzes for a user
+    static async getUserQuizzes(uid: string) {
+        const userDocumentReference = doc(db, 'users', uid);
+        const quizCollectionReference = collection(userDocumentReference, 'quizzes');
+        const quizCollection = await getDocs(quizCollectionReference);
 
-    quizCollection.forEach((doc) => {
-        if (doc.exists()) {
-            return doc.data();
-        } else {
-            console.log('Cannot get quiz.');
-            return null;
-        }
-    });
-};
+        quizCollection.forEach((doc) => {
+            if (doc.exists()) {
+                return doc.data();
+            } else {
+                console.log('Cannot get quiz.');
+                return null;
+            }
+        });
+    }
 
-/* Uses onSnapshot to capture a stream of the session and work with it. 
-Might be better to implement this more into the frontend, or in a separate file. */
+    /* Uses onSnapshot to capture a stream of the session and work with it. 
+    Might be better to implement this more into the frontend, or in a separate file. */
 
-const getSessionDocument = async (sessionid: string) => {
-    const sessionDocumentReference = doc(db, 'sessions', sessionid);
-    // const sessionDocument = await getDoc(sessionDocumentReference);
+    async getSessionDocument(sessionid: string) {
+        const sessionDocumentReference = doc(db, 'sessions', sessionid);
+        // const sessionDocument = await getDoc(sessionDocumentReference);
 
-    // if (sessionDocument.exists()) {
-    //     return sessionDocument.data();
-    // } else {
-    //     console.log('Cannot get session.');
-    //     return null;
-    // }
+        // if (sessionDocument.exists()) {
+        //     return sessionDocument.data();
+        // } else {
+        //     console.log('Cannot get session.');
+        //     return null;
+        // }
 
-    return onSnapshot(sessionDocumentReference, (session) => {
-        if (session.exists()) {
-            return session.data();
-        } else {
-            console.log('Cannot get session.');
-            return null;
-        }
-    });
-};
-
-export { getUserDocument, getUserQuizzes, getSessionDocument };
+        return onSnapshot(sessionDocumentReference, (session) => {
+            if (session.exists()) {
+                return session.data();
+            } else {
+                console.log('Cannot get session.');
+                return null;
+            }
+        });
+    }
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,23 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import "./Home.css"
 import logo_vite from "../../assets/vite.svg";
 import logo_react from "../../assets/react.svg";
 import logo_firebase from "../../assets/firebase.svg";
+import QuizFireStore from "../../firebase/firestore";
 
 const Home: FC = () => {
+
+  const [quiz, setQuiz] = useState([]);
+
+   const fetchFireStore = async () => {
+    console.log("Get firestore stuff")
+    setQuiz(([await QuizFireStore.getUserDocument("gclVWPbq3lDp2SE2VW3L")]) as Array<never>)
+  }
+
+  useEffect(() => {
+    fetchFireStore()
+  })
 
   const handleButtonClick = (/*ev: any*/) => {
 
@@ -21,9 +33,11 @@ const Home: FC = () => {
       <h1>Home</h1>  
       <p>Welcome to our quiz!</p>
       <Button onClick={handleButtonClick} color="red">Hello</Button>
+      <div>{quiz}</div>
     </div>
   );
 };
-export default Home;
 
+
+export default Home;
 
