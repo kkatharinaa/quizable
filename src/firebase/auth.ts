40a5e8ba5e22736bbacbcf645635f56170b4b098
sebi.tatar2@gmail.com <sebi.tatar2@gmail.com>
2024-04-12 12:27:24 +0200

    Fix bring back firestore files

---
+++ src/firebase/auth.ts
@@ -0,0 +1,5 @@
+// import app from './config';
+// import { getAuth, signOut, setPersistence, browserSessionPersistence, sendSignInLinkToEmail, onAuthStateChanged, createUserWithEmailAndPassword } from 'firebase/auth';
+
+// Initialize Auth
+// const auth = getAuth(app);
---
+++ src/firebase/config.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+
+const env_var = import.meta.env
+
+
+const firebaseConfig = {
+  apiKey: env_var.VITE_API_KEY,
+  authDomain: env_var.VITE_AUTH_DOMAIN,
+  databaseURL: env_var.VITE_DATABASE_URL,
+  projectId: env_var.VITE_PROJECT_ID,
+  storageBucket: env_var.VITE_STORAGE_BUCKET,
+  messagingSenderId: env_var.VITE_MESSAGING_SENDER_ID,
+  appId: env_var.VITE_APP_ID,
+  measurementId: env_var.VITE_MEASUREMENT_ID
+};
+
+console.log(firebaseConfig)
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
