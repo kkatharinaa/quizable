51ff22b3bd55fef627e6be1676d071ff2cfc6c67
Katharina <92579930+kkatharinaa@users.noreply.github.com>
2024-05-15 12:30:34 +0200

    [QZ-26] added UI for the endscreen on which the report can be sent

--- src/pages/Quiz/QuizEnd/QuizEnd.tsx
+++ src/pages/Quiz/QuizEnd/QuizEnd.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import "./QuizEnd.css"
 import {BottomNavBar} from "../../../components/BottomNavBar/BottomNavBar.tsx";
 import {BottomNavBarStyle, BottomNavBarType} from "../../../components/BottomNavBar/BottomNavBarExports.ts";
@@ -6,8 +6,18 @@ import {QuizCodeTag} from "../../../components/QuizCodeTag/QuizCodeTag.tsx";
 import {BackgroundGems} from "../../../components/BackgroundGems/BackgroundGems.tsx";
 import {BackgroundGemsType} from "../../../components/BackgroundGems/BackgroundGemsExports.ts";
 import {QuizMasterChildrenProps} from "../QuizMaster/QuizMaster.tsx";
+import {ButtonComponent} from "../../../components/Button/Button.tsx";
+import {SEND_ICON_LIGHT} from "../../../assets/Icons.ts";
+import {ButtonStyle, ButtonType} from "../../../components/Button/ButtonExports.ts";
 
 export const QuizEnd: FC<QuizMasterChildrenProps> = ({quizSessionManager, endQuizSession}) => {
+
+    const [sentReport, setSentReport] = useState(false)
+
+    const sendReport = () => {
+        setSentReport(true)
+        // TODO: send report to email
+    }
 
     return (
         <div className="quizEnd">
@@ -18,8 +28,19 @@ export const QuizEnd: FC<QuizMasterChildrenProps> = ({quizSessionManager, endQui
                 code={quizSessionManager.quizCode}
             />
             <div className="content">
-                <h1>Thank you for playing!</h1>
-                { /* TODO */}
+                <p>Would you like to get a report of the results via email?</p>
+                <div className={"buttonAndMessage"}>
+                    <ButtonComponent
+                        text={sentReport ? "Resend Report" : "Send Report"}
+                        icon={SEND_ICON_LIGHT}
+                        type={ButtonType.Long}
+                        style={ButtonStyle.Primary}
+                        onClick={sendReport}
+                    />
+                    { sentReport &&
+                        <p className={"infoMessage"}>We have sent you the report to your email!</p>
+                    }
+                </div>
             </div>
 
             <BottomNavBar
