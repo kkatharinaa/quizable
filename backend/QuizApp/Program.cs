4640e23a2cd7bb98a1356010d1f3e5bd8591979d
wired_less_maker <70153663+sebastianttr@users.noreply.github.com>
2024-05-28 21:43:05 +0200

    Updated the way we load environment variables in backend

--- backend/QuizApp/Program.cs
+++ backend/QuizApp/Program.cs
@@ -19,20 +19,26 @@ builder.Services.AddSingleton<IQuizSessionService, QuizSessionService>();
 builder.Services.AddSignalR();
 
 DotNetEnv.Env.Load();
+
+// 
+
 var port = Environment.GetEnvironmentVariable("PORT");
 if (!string.IsNullOrEmpty(port)) {
     builder.WebHost.UseUrls($"http://*:{port}");
 }
 
-ILogger logger = LoggerFactory.Create((lb) => {}).CreateLogger<Program>();
-
 var email_smtp = Environment.GetEnvironmentVariable("EMAIL_SMTP");
 var email_port = Environment.GetEnvironmentVariable("EMAIL_PORT");
 
-if(!string.IsNullOrEmpty(email_smtp) || !string.IsNullOrEmpty(email_smtp))
-    Console.WriteLine($"Email SMTP and Port are set. SMTP: {email_smtp}, Port: {email_port}");   
-else
-    Console.WriteLine("Email SMTP and Port are not set. Please set them in the environment variables.");
+if(string.IsNullOrEmpty(email_smtp) || string.IsNullOrEmpty(email_smtp))
+{
+    DotNetEnv.Env.Load("../../");
+
+    if(string.IsNullOrEmpty(Environment.GetEnvironmentVariable("EMAIL_SMTP"))){
+        throw new Exception("Cannot load environment variables. Please check if .env file in root exists.");
+    }
+}
+    
 
 var app = builder.Build();
 
--- src/services/QuizSessionService.ts
+++ src/services/QuizSessionService.ts
@@ -3,11 +3,13 @@ import QuizSession from "../models/QuizSession";
 import { Question } from "../models/Question";
 import QuizUser from "../models/QuizUser";
 
+const env_var = import.meta.env
+
 export default class QuizSessionService{
    
     //static port: number = 5296
     //static url: string = `http://localhost:${this.port}`
-    public static url: string = 'https://quizable-rjjp3736fa-lm.a.run.app'
+    public static url: string = env_var.VITE_API_URL
 
     public static async addSession(quizSession: QuizSession): Promise<string> {
         return (await fetch(`${this.url}/api/session`, 
